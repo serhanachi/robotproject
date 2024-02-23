1fc6ea423d6597a14f08b9414a306562443de087
Shala Froschheiser <161020412+serhanachi@users.noreply.github.com>
2024-02-23 12:04:07 +0200

    Create maincode.py

---
+++ maincode.py
@@ -0,0 +1,27 @@
+    import pigpio
+except ModuleNotFoundError as e:
+    from modules.mocks.mock_pigpio import MockPiGPIO
+    import pigpio
+    from modules.mocks.mock_cv2 import MockCV2
+
+from time import sleep, time
+import signal
+import schedule
+
+
+if Config.get('vision', 'tech') == 'opencv':
+    from modules.opencv.vision import Vision
+    from modules.opencv.tracking import Tracking
+    from modules.opencv.train_model import TrainModel
+    from modules.opencv.video_stream import VideoStream
+    from modules.opencv.timelapse import Timelapse
+elif Config.get('vision', 'tech') == 'coral':
+    from modules.coral.vision import Vision
+    from modules.coral.tracking import Tracking
+
+
+
+def mode():
+    if len(sys.argv) > 1 and sys.argv[1] == 'manual':
+        return Config.MODE_KEYBOARD
+    return Config.MODE_LIVE
